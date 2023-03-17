43b906d18bbff064962f6f4e41049b2ad281c628
Alissa <alissa.werbach@live.de>
2023-03-17 09:12:42 +0100

    fix authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,7 +8,7 @@ const authenticate = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" || !token) {
       return res.status(401).json({ message: "Not authorized" });
     }
     const { id } = jwt.verify(token, SECRET_KEY);
